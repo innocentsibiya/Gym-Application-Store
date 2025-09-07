83f8f6b6f20fd0a4fc1ecc2e5235bf5092540580
Innocent Sibiya <innoh.cbia@gmail.com>
2025-09-07 21:20:39 +0200

    cater for home and some code refactor

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,5 @@
-import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
-import { GymItem, GymItemService } from './services/gym-item-service.service';
-import { CardComponent } from './card/card.component';
-import { CartComponent } from './cart/cart.component';
-import { CartService } from './services/cart.service';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -10,28 +7,5 @@ import { CartService } from './services/cart.service';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'frontend';
-  items: GymItem[] = [];
-  cartItems: GymItem[] = [];
-  @ViewChild(CardComponent) childComponent!: CardComponent;
-  @ViewChild(CartComponent) cartComponent!: CartComponent;
-
-  constructor(private gymService: GymItemService,private cartService: CartService) {}
-
-  ngOnInit(): void {
-    this.gymService.getAll().subscribe(data => {
-      this.items = data;
-    });
-  }
-
-  showItemDetails(item: GymItem): void {
-    this.childComponent.openModal(item);
-  }
-
-  addItemToCart(item: GymItem): void {
-    this.cartService.addToCart(item).subscribe(cartItems => {
-      this.cartItems = cartItems;
-    });
-  }
 
 }
--- frontend/src/app/category/category.component.ts
+++ frontend/src/app/category/category.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { GymItem, GymItemService } from '../services/gym-item-service.service';
+import { CardComponent } from '../card/card.component';
+import { CartComponent } from '../cart/cart.component';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-category',
@@ -6,5 +10,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./category.component.less']
 })
 export class CategoryComponent {
+  items: GymItem[] = [];
+  cartItems: GymItem[] = [];
+  @ViewChild(CardComponent) childComponent!: CardComponent;
+  @ViewChild(CartComponent) cartComponent!: CartComponent;
 
+  constructor(private gymService: GymItemService,private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.gymService.getAll().subscribe(data => {
+      this.items = data;
+    });
+  }
+
+  showItemDetails(item: GymItem): void {
+    this.childComponent.openModal(item);
+  }
+
+  addItemToCart(item: GymItem): void {
+    this.cartService.addToCart(item).subscribe(cartItems => {
+      this.cartItems = cartItems;
+    });
+  }
 }
